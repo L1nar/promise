588b7e9af50b82fcc901d375de5c6230cf4f25ee
L1nar <122251664+L1nar@users.noreply.github.com>
2024-03-25 16:42:11 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,64 +1,98 @@
-const url1 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-019.jpg";
+const url1 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-01.jpg";
 const url2 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-023.jpg";
 const url3 = "http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-09.jpg";
 
-// const promie = {
-//     state: ['pending', 'fulfilled', 'rejected']
-// }
-
-// console.log('до promise');
+/* 
+const promise = {
+    state: ['pending', 'fulfilled', 'rejected']
+}
+ */
+// console.log('до промиса');
 // const promise = new Promise((resolve, reject) => {
-//     console.log('внутри promise');
-// })
-// console.log('после promise');
+//     console.log('внутри промиса')
+// });
+// console.log('после промиса');
 
 // const promise = new Promise((resolve, reject) => {
 //     resolve();
-// })
+// });
 
-// function delay(ms) {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//            resolve();
-//            console.log('resolved'); 
-//         }, ms);
-//     })
-// }
 
-// const promise = delay(2000);
+/* const promise = {
+    state: ['pending', 'fulfilled', 'rejected'],
+    resolveQueue: [
+        function () {
+            console.log('1');
+        },
+        function () {
+            console.log('2');
+        },
+        function () {
+            console.log('3');
+        }
+    ]
+} */
 
-// console.log('До then');
-// promise.then(() => console.log('Promise разрешился! 1'));
-// promise.then(() => console.log('Promise разрешился! 2'));
-// promise.then(() => console.log('Promise разрешился! 3'));
-// console.log('После then');
+/* function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+            console.log('resolved');
+        }, ms)
+    })
+}
 
-// promise
-//     .then(() => console.log('Promise разрешился! 1'))
-//     .then(() => console.log('Promise разрешился! 2'))
-//     .then(() => console.log('Promise разрешился! 3'));
+const promise = delay(2000);
+
+console.log('до then');
+promise.then(() => console.log('Промис разрешился! '));
+promise.then(() => console.log('Промис разрешился 2! '));
+promise.then(() => console.log('Промис разрешился 3! '));
+console.log('после then');
+
+promise
+    .then(() => console.log('Промис разрешился! '))
+    .then(() => console.log('Промис разрешился 2! '))
+    .then(() => console.log('Промис разрешился 3! ')); */
 
 function loadImage(url) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         const img = document.createElement('img');
         img.height = 200;
         img.src = url;
         document.body.append(img);
         img.addEventListener('load', () => {
             resolve();
-        })
+        });
         img.addEventListener('error', () => {
-            reject();
-        })
+            reject('Ошибка загрузки');
+        });
     })
 }
-loadImage(url1)
-    .then(() => {
-        return loadImage(url2);
-    })
-    .then(() => {
-        return loadImage(url3);
-    })
-    .then(() => {
+
+// loadImage(url1)
+//     .then(() => {
+//         return loadImage(url2);
+//     })
+//     .then(() => {
+//         return loadImage(url3);
+//     })
+//     .then(() => {
+//         console.log('Картинки загружены!');
+//     })
+//     .catch(() => {
+//         console.log('Ошибка загрузки');
+//     })
+
+async function fn() {
+    try {
+        await loadImage(url1);
+        await loadImage(url2);
+        await loadImage(url3);
         console.log('Картинки загружены');
-    })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+fn();
